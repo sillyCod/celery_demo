0a833045e724d29c5e01236305bae3c1752602fa
wangfusheng <wangfusheng@chuangxin.com>
2019-10-24 17:54:19 +0800

    fix redis and mongodb config.

--- tasks/config.py
+++ tasks/config.py
@@ -4,8 +4,8 @@ from kombu import Queue
 from kombu import Exchange
 
 result_serializer = "json"
-broker_url = "redis://localhost"
-result_backend = "mongodb://localhost/celery"
+broker_url = "redis://10.18.144.36"
+result_backend = "mongodb://10.18.144.36:27017/celery"
 timezone = "Asia/Shanghai"
 imports = (
     "tasks.trainer",
